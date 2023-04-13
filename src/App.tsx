361ef6607b06c273b1b1b0f831adcf2aa3f15031
Greg <gregcourt10@gmail.com>
2023-04-13 13:51:54 +0100

    adding firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import { Button } from '@mui/material';
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { SignInPage } from './conatiners/SignInPage';
 
 function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<SignInPage />}>
+        <Route path='/listings' element={<Listings />} />
+      </Route>
+    )
+  );
+
   return (
     <div className='bg-green-500'>
       <h1>Hello World</h1>

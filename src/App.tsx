a6f952e834c239017092d5da65c1ac7452f0011e
Greg <gregcourt10@gmail.com>
2023-04-13 13:34:22 +0100

    mui & tailwind injection set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <div className='bg-green-500'>
+      <h1>Hello World</h1>
+      <Button variant='contained' className='bg-red-500'>Testing MUI & Tailwind Injection</Button>
+    </div>
+  );
 }
 
 export default App;
